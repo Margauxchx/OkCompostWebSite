ef8cf5b9826fa6ee125f53e00edbc9810a4cfdf7
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-12-04 14:04:46 +0100

    [DB] mistake commit, nothing changed

--- db/migrate/20191203103004_create_contributions.rb
+++ db/migrate/20191203103004_create_contributions.rb
@@ -4,7 +4,11 @@ class CreateContributions < ActiveRecord::Migration[5.2]
       # N - 1 association with contributors
       t.belongs_to :contributor, index: true
       # N - 1 association with composts
+<<<<<<< Updated upstream
       t.belongs_to :supplied_compost, index:true
+=======
+      t.belongs_to :compost, index:true
+>>>>>>> Stashed changes
 
       t.datetime :contribution_date
 
