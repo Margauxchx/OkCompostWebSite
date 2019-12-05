025066720bc728e22489d353d40a9eff17f49184
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-12-05 15:15:16 +0100

    [ROUTES] Changed root to 'results#new'

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :results
+  root to: "results#new" 
+  resources :results, only: [:create, :new, :show]
   devise_for :users
   resources :users
   resources :composts
   resources :contributions
-  root to: "composts#index" 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
