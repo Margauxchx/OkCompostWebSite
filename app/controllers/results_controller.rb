84cd6d9675232f1fe73e802f1a8f588875038fe7
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-12-05 15:12:06 +0100

    [BACK] [SEARCH] Added strong parameters for district and composition to results controller

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,13 +2,11 @@ class ResultsController < ApplicationController
   before_action :set_result, only: [:show, :edit, :update, :destroy]
 
   # GET /results
-  # GET /results.json
   def index
     @results = Result.all
   end
 
   # GET /results/1
-  # GET /results/1.json
   def show
   end
 
@@ -22,42 +20,34 @@ class ResultsController < ApplicationController
   end
 
   # POST /results
-  # POST /results.json
   def create
     @result = Result.new(result_params)
 
     respond_to do |format|
       if @result.save
-        format.html { redirect_to @result, notice: 'Result was successfully created.' }
-        format.json { render :show, status: :created, location: @result }
+        format.html { redirect_to @result, notice: 'Voici les résultats de votre recherche :' }
       else
         format.html { render :new }
-        format.json { render json: @result.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /results/1
-  # PATCH/PUT /results/1.json
   def update
     respond_to do |format|
       if @result.update(result_params)
         format.html { redirect_to @result, notice: 'Result was successfully updated.' }
-        format.json { render :show, status: :ok, location: @result }
       else
         format.html { render :edit }
-        format.json { render json: @result.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /results/1
-  # DELETE /results/1.json
   def destroy
     @result.destroy
     respond_to do |format|
       format.html { redirect_to results_url, notice: 'Result was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -69,6 +59,8 @@ class ResultsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def result_params
-      params.fetch(:result, {})
+      params.require(:result).permit(
+        :district , :composition => []
+      )
     end
 end
