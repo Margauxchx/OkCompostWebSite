646b2b3a0af1d0bdc3bd415a2223fd1f6334deb7
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-12-05 15:15:47 +0100

    [DB] [SEARCH] Updated creation logic to fit search by district AND/OR composition

--- app/models/result.rb
+++ app/models/result.rb
@@ -7,24 +7,40 @@ class Result < ApplicationRecord
   # N - N association with composts, via result_lines
   has_many :composts, through: :result_lines
 
-  before_create :search_by_district, :search_by_composition
+  before_create :search
   after_create :save_lines
 
-  private
-
   def search_by_district
-    district = self.district
-    composts = Compost.tagged_with(district)
+    composts = Compost.tagged_with(self.district)
     composts.each do |compost|
       self.result_lines.new(compost_id: compost.id)
     end
   end
 
   def search_by_composition
-    composition = self.composition
-    composts = Compost.tagged_with(composition)
+    composts = Compost.tagged_with(self.composition)
     composts.each do |compost|
       self.result_lines.new(compost_id: compost.id)
+    end
+  end
+
+  def search_by_both
+    district_composts = Compost.tagged_with(self.district)
+    composts = district_composts.tagged_with(self.composition)
+    composts.each do |compost|
+      self.result_lines.new(compost_id: compost.id)
+    end
+  end
+
+  private
+
+  def search
+    if !self.district.empty? && self.composition.empty?
+      self.search_by_district
+    elsif self.district.empty? && self.composition.any?
+      self.search_by_composition
+    elsif !self.district.empty? && self.composition.any?
+      self.search_by_both
     end
   end
 
