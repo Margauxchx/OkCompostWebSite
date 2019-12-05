3e8848e416cd0c565a30d8a7d0ccef7a84d77ed5
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-12-05 15:03:33 +0100

    [SEARCH] [DB] Added result attributes and association

--- db/migrate/20191205070857_create_results.rb
+++ db/migrate/20191205070857_create_results.rb
@@ -1,6 +1,11 @@
 class CreateResults < ActiveRecord::Migration[5.2]
   def change
     create_table :results do |t|
+      # N -1 association with users
+      t.belongs_to :user            
+      
+      t.string :district
+      t.string :composition, array: true, default: []
 
       t.timestamps
     end
