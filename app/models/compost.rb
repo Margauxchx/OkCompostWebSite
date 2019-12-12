6ee25a14f0b9d5f12d8ee6f1e61d7befbe585618
Heroponne <florence.orgeret@gmail.com>
2019-12-12 16:19:32 +0100

    [BACK] edited validates

--- app/models/compost.rb
+++ app/models/compost.rb
@@ -6,8 +6,6 @@ validates :address, presence: true
 validates :zipcode, 
   presence: true,
   inclusion: { in: ["75001", "75002", "75003", "75004", "75005", "75006", "75007", "75008", "75009", "75010", "75011", "75012", "75013", "75014", "75015", "75016", "75017", "75018", "75019", "75020"] }
-validates :city, presence: true
-validates :country, presence: true
 validates :description,
   presence: true,
   length: { maximum: 160 }
