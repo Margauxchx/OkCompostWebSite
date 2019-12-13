fc61d8ed6f87cce8d5dcb95455a0710c36b6c622
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-12-13 09:23:45 +0100

    [DB] added compost association to timeslot table

--- db/migrate/20191213082157_create_timeslots.rb
+++ db/migrate/20191213082157_create_timeslots.rb
@@ -1,6 +1,8 @@
 class CreateTimeslots < ActiveRecord::Migration[5.2]
   def change
     create_table :timeslots do |t|
+      # N - 1 association with compost
+      t.belongs_to :compost, index: true
       t.integer :weekday
       t.string :start_time
       t.string :end_time
