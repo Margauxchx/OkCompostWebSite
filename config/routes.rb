8cfbbff4327a686eb208181806207b5cee5bfab2
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-12-13 16:31:52 +0100

    [BACK] Removed route leading to users index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "results#new" 
   resources :results, only: [:create, :new, :show]
   devise_for :users
-  resources :users
+  resources :users, except: [:index]
 
   resources :composts do
     resources :pictures, only: [:create]
