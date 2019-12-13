6861d7c68eb2346f4eb50999d0e567a54c12d3d9
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-12-13 09:22:22 +0100

    [SACCOFLD] [CALENDAR] generated timeslot model

---
+++ db/migrate/20191213082157_create_timeslots.rb
@@ -0,0 +1,11 @@
+class CreateTimeslots < ActiveRecord::Migration[5.2]
+  def change
+    create_table :timeslots do |t|
+      t.integer :weekday
+      t.string :start_time
+      t.string :end_time
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/timeslots.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :timeslot do
+    weekday { 1 }
+    start_time { "MyString" }
+    end_time { "MyString" }
+  end
+end
