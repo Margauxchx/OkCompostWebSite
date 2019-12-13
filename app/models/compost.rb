0f76ca6d85a22a7fdbb75601bae348d2e06fa5e8
Heroponne <florence.orgeret@gmail.com>
2019-12-13 09:44:33 +0100

    [BACK] edited error messages

--- app/models/compost.rb
+++ app/models/compost.rb
@@ -1,14 +1,14 @@
 class Compost < ApplicationRecord
 validates :title,
-  presence: true,
-  length: { in: 3..30 }
-validates :address, presence: true
+  presence: { message: "ne peut pas être vide"},
+  length: { in: 3..30, message: "a une longueur inappropriée" }
+validates :address, presence: { message: "ne peut pas être vide"}
 validates :zipcode, 
-  presence: true,
-  inclusion: { in: ["75001", "75002", "75003", "75004", "75005", "75006", "75007", "75008", "75009", "75010", "75011", "75012", "75013", "75014", "75015", "75016", "75017", "75018", "75019", "75020"] }
+  presence: { message: "ne peut pas être vide"},
+  inclusion: { in: ["75001", "75002", "75003", "75004", "75005", "75006", "75007", "75008", "75009", "75010", "75011", "75012", "75013", "75014", "75015", "75016", "75017", "75018", "75019", "75020"], message: "est invalide, il doit correspondre à un arrondissement parisien" }
 validates :description,
-  presence: true,
-  length: { maximum: 160 }
+  presence: { message: "ne peut pas être vide"},
+  length: { maximum: 160, message: "a une longueur inappropriée" }
 
   acts_as_taggable_on :districts, :compositions
 
